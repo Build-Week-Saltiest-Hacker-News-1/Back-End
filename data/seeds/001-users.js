48c6180df266af13a5193b2b09c7b58d2bc0dd90
Robert Campbell <Campbell.Rob@protonmail.com>
2020-01-06 13:30:26 -0800

    removed truncate in users and comments seed files

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,32 +1,28 @@
 exports.seed = function(knex, Promise) {
-  return knex("users")
-    .truncate()
-    .then(function() {
-      return knex("users").insert([
-        {
-          name: "Joe Biden",
-          user: "SaltiestManAlive",
-          email: "JoeB@democrats.com",
-          password: "bidenrules"
-        },
-        {
-          name: "Hillary Clinton",
-          user: "SaltiestWomanAlive",
-          email: "HillaryC@sidechick.com",
-          password: "billdrools"
-        },
-        {
-          name: "Papa John",
-          user: "PapaJohn",
-          email: "PizzaKing@bigpapaJ.com",
-          password: "pizza4lyfe"
-        },
-        {
-          name: "Donald J Drumpf",
-          user: "DonaldJDrumpf",
-          email: "PervySage@trump.com",
-          password: "obamadrools"
-        }
-      ]);
-    });
+  return knex("users").insert([
+    {
+      name: "Joe Biden",
+      user: "SaltiestManAlive",
+      email: "JoeB@democrats.com",
+      password: "bidenrules"
+    },
+    {
+      name: "Hillary Clinton",
+      user: "SaltiestWomanAlive",
+      email: "HillaryC@sidechick.com",
+      password: "billdrools"
+    },
+    {
+      name: "Papa John",
+      user: "PapaJohn",
+      email: "PizzaKing@bigpapaJ.com",
+      password: "pizza4lyfe"
+    },
+    {
+      name: "Donald J Drumpf",
+      user: "DonaldJDrumpf",
+      email: "PervySage@trump.com",
+      password: "obamadrools"
+    }
+  ]);
 };
