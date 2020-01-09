194861e3a9d44c6deee7910973e9cd6532ca4aba
Robert Campbell <Campbell.Rob@protonmail.com>
2020-01-09 10:51:22 -0800

    edit saltyRank.

--- data/seeds/003-feed.js
+++ data/seeds/003-feed.js
@@ -6,138 +6,138 @@ exports.seed = function(knex) {
       saltyComment:
         "That's basically what trusts are, with a few additional benefits.",
       comment_id: 21706756,
-      saltyRank: 0.420145247
+      saltyRank: 42
     },
     {
       saltyUsername: "ausjke",
       saltyComment:
         "Huawei had to license synopsys/cadence/mentor to design their chip(which is using ARM core), if US really wants to play hardball Huawei's IC design can be killed nearly instantly once those CAD design software is forbidden. In the meantime I heard Huawei etc bought a few years' license right before the trade war, they're actually well prepared. Another way is to restrict chip manufacturing, Taiwan pretty much owns the best technology for that(TSMC), Intel/Samsung has some, Huawei has none. And you have Android to some degree, without Android there is no Smartphone dominance from China as of now. Open source in general helped China to catch up and overtake the west extremely efficiently, you don't need any business spies anymore, just download/modify/profit. At the moment USA still has the possibility to contain IC ecosystem if it really wants to do that. In 10~20 years it will be a different story though, by then USA might not have much leverage if anything at all left.",
       comment_id: 21678985,
-      saltyRank: 0.550935673
+      saltyRank: 55
     },
     {
       saltyUsername: "mgolawala",
       saltyComment:
         '"Bad men need nothing more to compass their ends, than that good men should look on and do nothing" - John Stuart Mill. Often quoted as "The only thing necessary for the triumph of evil is for good men to do nothing". The world needs people like you to speak up, to keep people like them in check. Do it anonymously if it suits you, but please do it.',
       comment_id: 21705572,
-      saltyRank: 0.6232675895
+      saltyRank: 62
     },
     {
       saltyUsername: "dylanpyle",
       saltyComment:
         "A couple of years ago we lost our domain [1] due to a registrar (that we were not a customer of) erroneously issuing a suspension. The amount of honor system involved in the whole process, particularly in ccTLDs without as much oversight, was really surprising. [1]:  https://medium.com/thisiscala/the-duct-tape-holding-the-inte...",
       comment_id: 21703980,
-      saltyRank: 0.4868116
+      saltyRank: 48
     },
     {
       saltyUsername: "sedatk",
       saltyComment: "Maybe it\u2019s made of Markdown.",
       comment_id: 21690060,
-      saltyRank: 0.4063869214
+      saltyRank: 40
     },
     {
       saltyUsername: "pryelluw",
       saltyComment:
         'I used to work at the company (Mokriya) that had previously released an "official" CL app. CL\'s attitude towards apps was funny. I never got it. The lead dev told me they were hard to work with and were against providing APIs. No wonder they got their lunch eaten by facebook and other marketplaces.',
       comment_id: 21702814,
-      saltyRank: 0.5494644149
+      saltyRank: 54
     },
     {
       saltyUsername: "tcbasche",
       saltyComment: "GCS - not to be confused with Google Cloud Storage ;)",
       comment_id: 21697283,
-      saltyRank: 0.60671815
+      saltyRank: 60
     },
     {
       saltyUsername: "aklemm",
       saltyComment:
         "But definitely it\u2019s the girls\u2019 fault, am I right?",
       comment_id: 21699136,
-      saltyRank: 0.5875335297
+      saltyRank: 58
     },
     {
       saltyUsername: "arethuza",
       saltyComment:
         "The only people who generally have private health insurance in the UK (other than dental care) are those who work for companies where, usually senior, staff get this as a taxable part of their package. I wouldn't have thought that artists would belong to this category.",
       comment_id: 21683023,
-      saltyRank: 0.5164055892
+      saltyRank: 51
     },
     {
       saltyUsername: "awinder",
       saltyComment:
         "Kind-of alluded to in the article, but if you\u2019re going to unbundle AWS, you\u2019re going to have to figure out how to get the bandwidth 0-rated. One of amazons biggest moats (imo) is that they can realize monumental cost savings on bandwidth while making it very pricy to other service providers. So if you need to architect bandwidth-hungry or \u201chigh scale\u201d applications, going outside of AWS becomes way too expensive. Even privatelink tacks on bandwidth charges. You\u2019d have to look at DB providers like Elastic to see how to do this, I think. But it\u2019s going to involve a very complex and high touch deployment & support environment. This effectively \u201cprices out\u201d certain classes of applications as too expensive for value returned. Again I could be looking at this very wrong, and figuring out how to build profitable unbundled AWS services would be a great business, but it\u2019s hard to see how to successfully execute.",
       comment_id: 21675857,
-      saltyRank: 0.5533833304
+      saltyRank: 55
     },
     {
       saltyUsername: "1000units",
       saltyComment:
         'In a world where "telling" people is enough, there is no evil.',
       comment_id: 21697115,
-      saltyRank: 0.6738936357
+      saltyRank: 67
     },
     {
       saltyUsername: "layoutIfNeeded",
       saltyComment:
         "Sic transit gloria mundi ( \u0361\u00b0 \u035c\u0296 \u0361\u00b0)",
       comment_id: 21732153,
-      saltyRank: 0.3907887026
+      saltyRank: 39
     },
     {
       saltyUsername: "BeeOnRope",
       saltyComment:
         "A plain reading indicates that yes, he's only referring to the last benchmark, which showed the 2/3 disparity.",
       comment_id: 21726019,
-      saltyRank: 0.5093037138
+      saltyRank: 50
     },
     {
       saltyUsername: "techntoke",
       saltyComment:
         "Google has done some things well, but some things very poorly. First, fire whoever decided to prevent syncing Google Photos to Drive and not having a compatible Photos API for syncing original backups. Next, invest in and add features to Google Music or stop charging for it. Last, Google News sucks. No matter how much I dislike content it keeps showing me more of the same. Reminds me of YouTube. People should get heated on Google curating propaganda.",
       comment_id: 21697184,
-      saltyRank: 0.6081821038
+      saltyRank: 60
     },
     {
       saltyUsername: "asdff",
       saltyComment:
         "Who would have thought being told to corral like cattle under a table to rehearse waiting for rapid fire death wouldn't inspire much confidence.",
       comment_id: 21690689,
-      saltyRank: 0.5534297799
+      saltyRank: 55
     },
     {
       saltyUsername: "ngcc_hk",
       saltyComment:
         "Should say better and smarter management. It should not be accusing the solar panel.  Computer jeopardising a lot of things otherwise.",
       comment_id: 21678934,
-      saltyRank: 0.4994507989
+      saltyRank: 49
     },
     {
       saltyUsername: "52-6F-62",
       saltyComment:
         "In the past I've done a regular small bit off each pay to United Way here in Canada. Outside of that I like to give to Sick Kids Hospital in Toronto when I can (or take part in their fundraiser lottery). Also Heart and Stroke on occasion. I've previously volunteered through UN Online, though I'm not sure if that counts as donating in the strict sense. And Wikipedia at least once.",
       comment_id: 21693418,
-      saltyRank: 0.5698628381
+      saltyRank: 56
     },
     {
       saltyUsername: "mvanbaak",
       saltyComment:
         "If there's a way to bypass it, it will be used. By the authorized recyclers as well as the thieves (including government agencies).",
       comment_id: 21707152,
-      saltyRank: 0.4879038781
+      saltyRank: 48
     },
     {
       saltyUsername: "sacomo",
       saltyComment:
         "This is one of the most simplististic, reductionary arguments against socialism I have read. Anyone who uses the 'human nature' argument shouldn't be taken seriously. Especially with this article's mischaraterization of socialism in the first place. The idea that all of human nature can be boiled down to only the 'selfish' and 'greedy' traits is pretty insulting, and in this case, propagandistic. We really shouldn't continue nurturing the selfishness and greed traits on a mass scale as we currently do. Socialism is never about people working for free or forcing lazy people to work. Its about workers owning the means of production. Giving workers dignity and eliminating the oppresive employer - employee relationship. Slave owners also thought that they were unfairly being \"forced\" to give up their freedoms of owning people.",
       comment_id: 21726353,
-      saltyRank: 0.6366853987
+      saltyRank: 63
     },
     {
       saltyUsername: "lern_too_spel",
       saltyComment:
         "You're right. Apple is worse. They show  only  Apple News results.",
       comment_id: 21718543,
-      saltyRank: 0.6636544068
+      saltyRank: 66
     }
   ]);
 };
